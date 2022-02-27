05981d3002d30ffe7270e8715c191e321a3d1a30
monkeycs60 <clement.serizay@gmail.com>
2022-02-27 23:23:07 +0100

    js learn

---
+++ truc.js
@@ -0,0 +1,11 @@
+// var meilleurPresident = ["Hollande", "Sarko", "Macron", "Chirac"]
+// meilleurPresident[2];
+// var lePire = "le meilleur Président est " + meilleurPresident[1];
+// alert(lePire)
+// var nouveauPresident = meilleurPresident.slice(1, 3);
+// if (nouveauPresident == "Sarko" && "Macron") {
+//     alert("nous sommes perdus")
+// }
+// else {
+//     alert("Encore un espoir")
+// }
